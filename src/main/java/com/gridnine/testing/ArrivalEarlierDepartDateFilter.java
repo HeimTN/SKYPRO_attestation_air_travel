9974c988f4046367639e7ed68d020cc1384da0ce
ZaretskiyM <zaretwork@gmail.com>
2024-01-10 15:57:39 +0300

    Add JavaDoc

--- src/main/java/com/gridnine/testing/ArrivalEarlierDepartDateFilter.java
+++ src/main/java/com/gridnine/testing/ArrivalEarlierDepartDateFilter.java
@@ -2,13 +2,27 @@ package com.gridnine.testing;
 
 import java.util.List;
 import java.util.stream.Collectors;
-
+/**
+ * The {@code ArrivalEarlierDepartDateFilter} class implements the {@code AirTravelFilter} interface
+ * and filters flights based on the criteria that all segments must have an arrival date later than the departure date.
+ */
 public class ArrivalEarlierDepartDateFilter implements AirTravelFilter{
+    /**
+     * Gets the name of the filter.
+     *
+     * @return The name of the filter: "Filter: Segments with arrival date earlier than departure date".
+     */
     @Override
     public String filterName() {
         return "Filter: Segments with arrival date earlier than departure date";
     }
-
+    /**
+     * Filters a list of flights, retaining only those where all segments have arrival dates later than
+     * the corresponding departure dates.
+     *
+     * @param flights The list of flights to be filtered.
+     * @return A new list of flights with segments having arrival dates later than departure dates.
+     */
     @Override
     public List<Flight> filteredAirTravels(List<Flight> flights) {
         return flights.stream()
--- src/main/java/com/gridnine/testing/DepartCurrentTimePointFilter.java
+++ src/main/java/com/gridnine/testing/DepartCurrentTimePointFilter.java
@@ -3,13 +3,28 @@ package com.gridnine.testing;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
-
+/**
+ * The {@code DepartCurrentTimePointFilter} class implements the {@code AirTravelFilter} interface
+ * and filters flights based on the departure time criteria up to the current point in time.
+ */
 public class DepartCurrentTimePointFilter implements AirTravelFilter{
+    /**
+     * Gets the name of the filter.
+     *
+     * @return The name of the filter: "Filter: Departure up to the current point in time".
+     */
     @Override
     public String filterName() {
         return "Filter: Departure up to the current point in time";
     }
 
+    /**
+     * Filters a list of flights, retaining only those where all segments have departure times after
+     * the current point in time.
+     *
+     * @param flights The list of flights to be filtered.
+     * @return A new list of flights with segments departing after the current point in time.
+     */
     @Override
     public List<Flight> filteredAirTravels(List<Flight> flights) {
         LocalDateTime time = LocalDateTime.now();
